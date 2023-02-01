1a4ebe9a0b281e4be8c436e894a54a40780e0843
Adam Measel <ameasel55@gmail.com>
2023-02-01 14:05:32 -0600

    methods

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace MethodsExercise
+{
+	public class Methods
+	{
+		
+	
+			public static int Add(int num1, int num2)
+			{
+				return num1 + num2;
+			}
+			public static int Subtract(int num3, int num4)
+			{
+				return num3 - num4;
+			}
+			public static int Multiply(int num5, int num6)
+			{
+				return num5 * num6;
+			}
+			public static int Divide(int num7, int num8)
+			{
+				return num7 / num8;
+			}
+			public static int Modulus(int num9, int num10)
+			{
+			return num9 % num10;
+			}
+	}
+}
+
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite colour");
+            string colour = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name} my name is also {name}. What is your favorite " +
+                $"colour? Woah {colour} is my favorite colour too! " +
+                $"If I had to guess I'd say your favorite animal is probably a {animal}?? " +
+                $"I knew it!! Ok so what is your favorite band? OMG I love {band} " +
+                $"I listen to them all the time!!");
+
+
+            Methods.Add(22, 45);
+            Console.WriteLine(Methods.Add(22, 45));
+
+            Methods.Subtract(534, 298);
+            Console.WriteLine(Methods.Subtract(534, 298));
+
+            Methods.Multiply(6, 333);
+            Console.WriteLine(Methods.Multiply(6, 333));
+
+            Methods.Divide(100, 5);
+            Console.WriteLine(Methods.Divide(100, 5));
+
+            Methods.Modulus(55, 601);
+            Console.WriteLine(Methods.Modulus(55, 601));
+
+
         }
     }
 }
